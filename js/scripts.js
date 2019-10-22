fd6023e747b79421883730cc585f937351625748
hristo dzhambov and maria lagunero <example@example.com>
2019-10-22 15:55:19 -0700

    add change hide/ show to toggle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
   $("button#JavaScript").click(function() {
-    $("#JavaScript-showing").show();
-  
+    $("#JavaScript-showing").toggle();
+    $("#JavaScript-hide").toggle();
+  });
+});
 
+$(document).ready(function() {
+  $("button#Operators").click(function() {
+    $("#Operators-showing").toggle();
+    $("#Operators-hide").toggle();
   });
 });
